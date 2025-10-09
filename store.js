bfeee31ac91b9464a854563bdbfb92568ca38124
felisarmp4 <felisayereyes@gmail.com>
2025-10-09 11:01:34 -0700

    bl;pu9p97p

---
+++ store.js
@@ -0,0 +1,10 @@
+const product = [
+    { name: "full lemon", price: 0.99, img: "1.png"},
+    { name: "full lime", price: 0.99, img: "2.png"},
+    { name: "full lemon and a slice", price: 1.39, img: "3.png"},
+    { name: "full lime and a slice", price: 1.39, img: "4.png"},
+    { name: "crate of lemons", price: 44.99, img: "5.png"},
+    { name: "crate of limes", price: 44.99, img: "6.png"},
+    { name: "lemon meringue pie", price: 44.99, img: "7.png"},
+    { name: "key lime pie", price: 44.99, img: "8.png"},
+]
