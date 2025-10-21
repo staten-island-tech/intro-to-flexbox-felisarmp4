540fbd85a3d69315ddd3deca7b9a557dc5699c10
felisarmp4 <felisayereyes@gmail.com>
2025-10-21 11:02:19 -0700

    we4rtf54y56u7

--- store.js
+++ store.js
@@ -1,24 +1,24 @@
 const products = [
-    { name: "full lemon", price: 0.99, img: "1.png"},
-    { name: "full lime", price: 0.99, img: "2.png"},
-    { name: "full lemon and a slice", price: 1.39, img: "3.png"},
-    { name: "full lime and a slice", price: 1.39, img: "4.png"},
-    { name: "crate of lemons", price: 44.99, img: "5.png"},
-    { name: "crate of limes", price: 44.99, img: "6.png"},
-    { name: "lemon meringue pie", price: 4.99, img: "7.png"},
-    { name: "key lime pie", price: 4.99, img: "8.png"},
+    { name: "full lemon", price: 0.99, img: "1.png", type: "fresh fruit"},
+    { name: "full lime", price: 0.99, img: "2.png", type: "fresh fruit"},
+    { name: "full lemon and a slice", price: 1.39, img: "3.png", type: "fresh fruit"},
+    { name: "full lime and a slice", price: 1.39, img: "4.png", type: "fresh fruit"},
+    { name: "crate of lemons", price: 44.99, img: "5.png", type: "fresh fruit"},
+    { name: "crate of limes", price: 44.99, img: "6.png", type: "fresh fruit"},
+    { name: "lemon meringue pie", price: 4.99, img: "7.png", type: "baked goods"},
+    { name: "key lime pie", price: 4.99, img: "8.png", type: "baked goods"},
     { name: "lemonade", price: 3.99, img: "9.png"},
     { name: "limeade", price: 3.99, img: "10.png"},
-    { name: "lemon pound cake", price: 14.99, img: "11.png"},
-    { name: "key lime pound cake", price: 14.99, img: "12.png"},
+    { name: "lemon pound cake", price: 14.99, img: "11.png", type: "baked goods"},
+    { name: "key lime pound cake", price: 14.99, img: "12.png", type: "baked goods"},
     { name: "outshine lemon fruit bars", price: 5.99, img: "13.png"},
     { name: "outshine lime fruit bars", price: 5.99, img: "14.png"},
     { name: "lemon drops", price: 1.99, img: "15.png"},
     { name: "lime drops", price: 1.99, img: "16.png"},
     { name: "lemon drop jello shots", price: 3.99, img: "17.png"},
     { name: "lime drop jello shots", price: 3.99, img: "18.png"},
-    { name: "lemon macarons", price: 19.99, img: "19.png"},
-    { name: "lime macarons", price: 19.99, img: "20.png"}
+    { name: "lemon macarons", price: 19.99, img: "19.png", type: "baked goods"},
+    { name: "lime macarons", price: 19.99, img: "20.png", type: "baked goods"}
 ];
 const container = document.getElementById('product-list');
 
@@ -35,3 +35,5 @@ products.forEach(product => {
     </div>`
   );
 });
+
+function filterby
