0e9f2764a1ac32db479409f4a4e6456d3ae4e22b
felisarmp4 <felisayereyes@gmail.com>
2025-10-23 11:04:28 -0700

    rfw3r3r

--- store.js
+++ store.js
@@ -1,30 +1,78 @@
 const products = [
-    { name: "full lemon", price: 0.99, img: "1.png", type: "fresh fruit"},
-    { name: "full lime", price: 0.99, img: "2.png", type: "fresh fruit"},
-    { name: "full lemon and a slice", price: 1.39, img: "3.png", type: "fresh fruit"},
-    { name: "full lime and a slice", price: 1.39, img: "4.png", type: "fresh fruit"},
-    { name: "crate of lemons", price: 44.99, img: "5.png", type: "fresh fruit"},
-    { name: "crate of limes", price: 44.99, img: "6.png", type: "fresh fruit"},
-    { name: "lemon meringue pie", price: 4.99, img: "7.png", type: "baked goods"},
-    { name: "key lime pie", price: 4.99, img: "8.png", type: "baked goods"},
-    { name: "lemonade", price: 3.99, img: "9.png", type: "drinks"},
-    { name: "limeade", price: 3.99, img: "10.png", type: "drinks"},
-    { name: "lemon pound cake", price: 14.99, img: "11.png", type: "baked goods"},
-    { name: "key lime pound cake", price: 14.99, img: "12.png", type: "baked goods"},
-    { name: "outshine lemon fruit bars", price: 5.99, img: "13.png", type: "sweets and snacks"},
-    { name: "outshine lime fruit bars", price: 5.99, img: "14.png", type: "sweets and snacks"},
-    { name: "lemon drops", price: 1.99, img: "15.png", type: "sweets and snacks"},
-    { name: "lime drops", price: 1.99, img: "16.png", type: "sweets and snacks"},
-    { name: "lemon drop jello shots", price: 3.99, img: "17.png", type: "sweets and snacks"},
-    { name: "lime drop jello shots", price: 3.99, img: "18.png", type: "sweets and snacks"},
-    { name: "lemon macarons", price: 19.99, img: "19.png", type: "baked goods"},
-    { name: "lime macarons", price: 19.99, img: "20.png", type: "baked goods"}
+  { name: "full lemon", price: 0.99, img: "1.png", type: "fresh fruit" },
+  { name: "full lime", price: 0.99, img: "2.png", type: "fresh fruit" },
+  {
+    name: "full lemon and a slice",
+    price: 1.39,
+    img: "3.png",
+    type: "fresh fruit",
+  },
+  {
+    name: "full lime and a slice",
+    price: 1.39,
+    img: "4.png",
+    type: "fresh fruit",
+  },
+  { name: "crate of lemons", price: 44.99, img: "5.png", type: "fresh fruit" },
+  { name: "crate of limes", price: 44.99, img: "6.png", type: "fresh fruit" },
+  {
+    name: "lemon meringue pie",
+    price: 4.99,
+    img: "7.png",
+    type: "baked goods",
+  },
+  { name: "key lime pie", price: 4.99, img: "8.png", type: "baked goods" },
+  { name: "lemonade", price: 3.99, img: "9.png", type: "drinks" },
+  { name: "limeade", price: 3.99, img: "10.png", type: "drinks" },
+  {
+    name: "lemon pound cake",
+    price: 14.99,
+    img: "11.png",
+    type: "baked goods",
+  },
+  {
+    name: "key lime pound cake",
+    price: 14.99,
+    img: "12.png",
+    type: "baked goods",
+  },
+  {
+    name: "outshine lemon fruit bars",
+    price: 5.99,
+    img: "13.png",
+    type: "sweets and snacks",
+  },
+  {
+    name: "outshine lime fruit bars",
+    price: 5.99,
+    img: "14.png",
+    type: "sweets and snacks",
+  },
+  {
+    name: "lemon drops",
+    price: 1.99,
+    img: "15.png",
+    type: "sweets and snacks",
+  },
+  { name: "lime drops", price: 1.99, img: "16.png", type: "sweets and snacks" },
+  {
+    name: "lemon drop jello shots",
+    price: 3.99,
+    img: "17.png",
+    type: "sweets and snacks",
+  },
+  {
+    name: "lime drop jello shots",
+    price: 3.99,
+    img: "18.png",
+    type: "sweets and snacks",
+  },
+  { name: "lemon macarons", price: 19.99, img: "19.png", type: "baked goods" },
+  { name: "lime macarons", price: 19.99, img: "20.png", type: "baked goods" },
 ];
-const container = document.getElementById('product-list');
+const container = document.getElementById("product-list");
 
-
-products.forEach(product => {
-
+products.forEach((product) => {
   container.insertAdjacentHTML(
     "beforeend",
     `<div class="display-card">
@@ -35,4 +83,3 @@ products.forEach(product => {
     </div>`
   );
 });
-
