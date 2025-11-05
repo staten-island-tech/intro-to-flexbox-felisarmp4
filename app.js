0f4ff3d1ed45b94362036afdd63d908029cd74c7
felisarmp4 <felisayereyes@gmail.com>
2025-11-05 11:03:18 -0800

    rgt54

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const age = 35;
+/*const age = 35;
 const citizen = true;
 // 2 equals evaulates and ignore type
 // if 1 == "1"
@@ -6,10 +6,9 @@ const citizen = true;
 //if 1 == "1"
 
 if (age === true && citizen === true) {
-
 }
 
-/*const age = 18;
+const age = 18;
 if (age>=18) {
     console.log("vote");
 } else {
@@ -96,4 +95,42 @@ parkingspace("CC..C.CC..")
 function whohaswand (start, num, ...duels){
     for()
 }
-console.log */
+console.log
+
+function total(cart) {
+  let total = 0;
+  cart.forEach((item) => {
+    if (item.price < 5) {
+      total += item.quanity * item.price * 0.095;
+    } else {
+      total += item.quanity * item.price;
+    }
+  });
+  if (total > 100) {
+    total = total * 0.9;
+  }
+  return total;
+} */
+
+const dnsrecords = [
+  { address: "amazon.com", dns: "205.251.242.103" },
+  { address: "apple.com", dns: "17.253.144.10" },
+  { address: "bbc.com", dns: "151.101.64.81" },
+  { address: "chat.openai.com", dns: "104.18.12.123" },
+  { address: "cnn.com", dns: "151.101.1.67" },
+  { address: "discord.com", dns: "162.159.137.232" },
+  { address: "github.com", dns: "140.82.113.3" },
+  { address: "google.com", dns: "142.250.72.14" },
+  { address: "instagram.com", dns: "157.240.229.174" },
+  { address: "microsoft.com", dns: "40.113.200.201" },
+  { address: "netflix.com", dns: "52.41.250.12" },
+  { address: "nasa.gov", dns: "198.49.245.141" },
+  { address: "nytimes.com", dns: "151.101.1.164" },
+  { address: "reddit.com", dns: "151.101.1.140" },
+  { address: "roblox.com", dns: "128.116.114.3" },
+  { address: "stanford.edu", dns: "171.67.215.200" },
+  { address: "youtube.com", dns: "142.250.190.46" },
+];
+
+const min = dnsrecords[0];
+const max = dnsrecords[dnsrecords.length - 1];
